fa2158d31e8c63990e3f1fe1822ff1e87030abdb
Sumia EOs <seosultra@gmail.com>
2022-09-20 22:52:33 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.at(pos);
+    }
+  }
 
-  max() {}
+  max(n) {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.length) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b);
+      //Math.min(...this.items);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
